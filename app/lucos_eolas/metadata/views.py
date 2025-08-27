f2dbefa0188a74b58aea3d7f7b4fc2a2c5a4f100
Luke Blaney <git@lukeblaney.co.uk>
2025-08-28 00:38:55 +0100

    add /_info endpoint

--- app/lucos_eolas/metadata/views.py
+++ app/lucos_eolas/metadata/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+def info(request):
+	output = {
+		'system': "lucos_eolas",
+		'ci': {
+			'circle': "gh/lucas42/lucos_eolas",
+		},
+		'network_only': True,
+		'title': "Eolas",
+		'show_on_homepage': True
+	}
+	return JsonResponse(output)
--- app/lucos_eolas/urls.py
+++ app/lucos_eolas/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from lucos_eolas.lucosauth import views as auth_views
+from lucos_eolas.metadata import views as metadata_views
 
 admin.site.login = auth_views.loginview
 
 urlpatterns = [
+    re_path(r'^_info$', metadata_views.info),
     path('', admin.site.urls),
     # Static files are handled by nginx at /resources, so not listed here
 ]
