2d7727c964c878f0a5a71bc07c9a543d395cd3a7
Luke Blaney <git@lukeblaney.co.uk>
2025-08-27 22:32:57 +0100

    serve admin UI at root path, rather than /admin

--- app/lucos_eolas/urls.py
+++ app/lucos_eolas/urls.py
@@ -1,21 +1,10 @@
-from django.urls import re_path, include
 from django.contrib import admin
 from django.urls import path
-from django.shortcuts import redirect
-from django.utils.http import urlencode
 from lucos_eolas.lucosauth import views as auth_views
 
-
-
-"""Send all admin login attempts straight to /accounts/login."""
-def direct_admin_login(request, extra_context=None):
-    params = urlencode({"next": request.GET.get("next", "/admin/")})
-    return redirect(f"/accounts/login/?{params}")
-
-admin.site.login = direct_admin_login
+admin.site.login = auth_views.loginview
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    re_path(r'^accounts/login/', auth_views.loginview),
-    # Static files are handled by nginx so not listed here
+    path('', admin.site.urls),
+    # Static files are handled by nginx at /resources, so not listed here
 ]
