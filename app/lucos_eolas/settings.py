327baf3cd79c0d2679ba36a5572635cb44799849
Luke Blaney <git@lukeblaney.co.uk>
2025-08-28 01:25:33 +0100

    fix paths for static resources

--- app/lucos_eolas/settings.py
+++ app/lucos_eolas/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/resources/'
-STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
