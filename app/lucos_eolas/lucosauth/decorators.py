f7c3e0f4c172513bca6073cbf7bbc2e567ffd5e6
Luke Blaney <git@lukeblaney.co.uk>
2025-09-05 02:55:50 +0100

    Return a 401 response when no Authorization header is given for endpoints which use API auth

--- app/lucos_eolas/lucosauth/decorators.py
+++ app/lucos_eolas/lucosauth/decorators.py
@@ -5,16 +5,17 @@ from django.http import HttpResponse
 def api_auth(func):
 	@wraps(func)
 	def _decorator(request, *args, **kwargs):
-		if 'HTTP_AUTHORIZATION' in request.META:
-			try:
-				authmeth, auth = request.META['HTTP_AUTHORIZATION'].split(' ', 1)
-			except ValueError:
-				return HttpResponse(status=400)
-			if authmeth.lower() == 'key':
-				user = getUserByKey(apikey=auth)
-				if user:
-					request.user = user
-				else:
-					return HttpResponse(status=403)
+		if 'HTTP_AUTHORIZATION' not in request.META:
+			return HttpResponse(status=401)
+		try:
+			authmeth, auth = request.META['HTTP_AUTHORIZATION'].split(' ', 1)
+		except ValueError:
+			return HttpResponse(status=400)
+		if authmeth.lower() == 'key':
+			user = getUserByKey(apikey=auth)
+			if user:
+				request.user = user
+			else:
+				return HttpResponse(status=403)
 		return func(request, *args, **kwargs)
 	return _decorator
