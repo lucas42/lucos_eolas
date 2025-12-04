256ef12b9d653adea42ac179d12d620c23b390d0
Luke Blaney <git@lukeblaney.co.uk>
2025-12-04 12:07:31 +0000

    include 'django.contrib.postgres' in INSTALLED_APPS so ArrayField works in Django 6

--- app/lucos_eolas/settings.py
+++ app/lucos_eolas/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
     'lucos_eolas.lucosauth',
     'lucos_eolas.metadata',
 ]
